fa51d37bd6450413b96592e680f8382ba3607b00
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-10-15 15:02:33 +0900

    Add: Django URL

---
+++ v2/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'v2'
+urlpatten = [
+  path('get_all_sensor/', views.get_all_sensor.as_view(), name='index'),
+  path('humidity/', views.humidity.as_view(), name='index'),
+  path('temp/', views.temp.as_view(), name='index'),
+  path('led/', views.led.as_view(), name='index'),
+  path('water/', views.water.as_view(), name='index'),
+  path('control_led/', views.control_led.as_view(), name='index'),
+  path('control_water/', views.control_water.as_view(), name='index')
+]
--- v2/views.py
+++ v2/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt  # csrf 비활성화 라이브러리
+from django.utils.decorators import method_decorator  # csrf 비활성화를 위한 메서드, 클래스 데코레이터
+from .MQTT.publish import *
+from .MQTT.subscribe import *
 
-# Create your views here.
+from .__init__ import recv
+senserValue = recv()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class get_all_sensor(View):
+  def get(self, request):
+    returnValue = {
+      'temp': {
+        'status': 0,  # -1 or 0 or 1 센서 값에 따라 부족, 적당, 높음의 기준을 나눔
+        'value': senserValue['temp']
+      },
+      'humidity': {
+        'status': 0,
+        'value': senserValue['humidity']
+      }
+    }
+    
+    if (senserValue['led_status'] == 1) and (senserValue['water_status'] == 1):
+      returnValue['led_status'] = {'status': True}
+      returnValue['water_status'] = {'status': True}
+      
+    elif (senserValue['led_status'] == 0) and (senserValue['water_status'] == 1):
+      returnValue['led_status'] = {'status': False}
+      returnValue['water_status'] = {'status': True}
+      
+    elif (senserValue['led_status'] == 1) and (senserValue['water_status'] == 0):
+      returnValue['led_status'] = {'status': True}
+      returnValue['water_status'] = {'status': False}
+      
+    elif (senserValue['led_status'] == 0) and (senserValue['water_status'] == 0):
+      returnValue['led_status'] = {'status': False}
+      returnValue['water_status'] = {'status': False}
+      
+    return JsonResponse(returnValue)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class temp(View):
+  def get(self, request):
+    returnValue = {
+      'status': 0,
+      'value': senserValue['temp']
+    }
+    return JsonResponse(returnValue)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class humidity(View):
+  def get(self, request):
+    returnValue = {
+      'status': 0,
+      'value': senserValue['humidity']
+    }
+    return JsonResponse(returnValue)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class led(View):
+  def get(self, request):
+    returnValue = {
+      'value': senserValue['led_status']
+    }
+    return JsonResponse(returnValue)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class water(View):
+  def get(self, request):
+    returnValue = {
+      'value': senserValue['water_status']
+    }
+    return JsonResponse(returnValue)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class control_water(View):
+  def post(self, request):
+    try:
+      if request.META['CONTENT_TYPE'] == 'application/json':
+        request = json.loads(request.body)
+        water_status = request['status']
+      else:
+        water_status = request.POST['status']
+      mqtt = mqtt_publish()
+      mqtt.led(water_status)
+      return HttpResponse('OK', status=200)
+    
+    except:
+      return HttpResponse('UNKNOWN SERVER ERROR ACCORDED', status=500)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class control_led(View):
+  def post(self, request):
+    try:
+      if request.META['CONTENT_TYPR'] == 'application/json':
+        request = json.loads(request.body)
+        led_status = request['status']
+      else:
+        led_status = request.POST['status']
+      mqtt = mqtt_publish()
+      mqtt.led(led_status)
+      return HttpResponse('OK', status=200)
+    
+    except:
+      return HttpResponse('UNKNOWN SERVER ERROR ACCORDED', status=500)
